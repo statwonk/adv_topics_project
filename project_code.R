21ce5afcca330292e14ba71f4bb26a6b7f79ccef
cpeter9 <Peters>
2013-03-14 20:16:41 -0500

    Added link to Github account.

--- project_code.R
+++ project_code.R
@@ -1,7 +1,7 @@
 # Advanced topics project
 # Authors: Chris Peters & Chengfei Lu
 # Purpose: Advanced topics class project
-#
+# https://github.com/cpeter9/adv_topics_project
 # Link to data: http://archive.ics.uci.edu/ml/datasets/Concrete+Compressive+Strength
 
 data <- read.csv("Concrete_Data.csv")

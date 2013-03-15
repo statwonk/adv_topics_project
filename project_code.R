5c88db4a9efb1e7daaea2c4c1448429bf5fb8ad8
cpeter9 <Peters>
2013-03-14 20:15:08 -0500

    I downloaded the data, changed it from xls to csv, changed the headers to generic names, and this file imports that data.

---
+++ project_code.R
@@ -0,0 +1,7 @@
+# Advanced topics project
+# Authors: Chris Peters & Chengfei Lu
+# Purpose: Advanced topics class project
+#
+# Link to data: http://archive.ics.uci.edu/ml/datasets/Concrete+Compressive+Strength
+
+data <- read.csv("Concrete_Data.csv")

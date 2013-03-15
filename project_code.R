50cb6d4468d8004cd8136d668f3a280f465ccbac
cpeter9 <Peters>
2013-03-14 20:25:51 -0500

    Help messages.

--- project_code.R
+++ project_code.R
@@ -4,4 +4,11 @@
 # https://github.com/cpeter9/adv_topics_project
 # Link to data: http://archive.ics.uci.edu/ml/datasets/Concrete+Compressive+Strength
 
-data <- read.csv("Concrete_Data.csv") #this is cool
+data <- read.csv("Concrete_Data.csv") # this line enters data
+
+# Look at the data with this command
+str(data)
+
+names(data)[1:3] <- c("Name_1", "Name_2", "Name_3") # this renames variables
+
+head(data, 3) # This shows you the first 'x' number of rows of the data

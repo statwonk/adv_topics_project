4620287af6584676d21d8fc6a3b9a818a89b4642
cpeter9 <Peters>
2013-03-15 11:27:00 -0500

    Added two ggplots, and also a random survival forest treating compressive strength as time-to-event.

--- project_code.R
+++ project_code.R
@@ -7,15 +7,59 @@
 data <- read.csv("Concrete_Data.csv") # this line enters data
 
 # Look at the data with this command
-str(data)
+# str(data)
 
-names(data)[1:3] <- c("Name_1", "Name_2", "Name_3") # this renames variables
+names(data) <- c("cement", "slag", "fly_ash", "water", "plasticizer", "course_aggregate", "fine_aggregate", "age", "comp_strength") # this renames variables
 
 head(data, 3) # This shows you the first 'x' number of rows of the data
 
-# this is comments from Chengfei
+
+#############################
+# Visualization and graphing
+############################
+
+# If you do not have ggplot2 install, please uncomment this line and install:
+# install.packages("ggplot2")
+library(ggplot2)
+
+# Simple pairs analysis
+pairs(data) # many non-linear and weak relationships
+
+# A look at how each variable is associated with comp_strength
+# install.packages("reshape2")
+library(reshape2)
+
+df <- melt(data, c("comp_strength")) # transforms data, comp_strength by every other variable, this is a useful form for plotting
+
+# Plot of each variable against comp_strength
+ggplot(df, aes(x = comp_strength, y = value)) +
+  geom_smooth(colour = "red", size = 2) +
+  geom_point(alpha = 0.2) +
+  facet_wrap(~ variable, scale = "free")
+
+# Does logging help? Result: logging 'age' variable may be appropriate, not helpful for other variables
+df$value <- log(df$value)
+
+ggplot(df, aes(x = comp_strength, y = value)) +
+  geom_smooth(colour = "red", size = 2) +
+  geom_point(alpha = 0.2) +
+  facet_wrap(~ variable, scale = "free")
+
+################################
+# Modelling
+###############################
+
+# install.packages("randomForestSRC")
+# install.packages("survival")
+library(randomForestSRC)
+library(survival)
+
+# All variables are not censored, they have "died", thus censor == 1
+data$censor <- 1
+
+tree <- rfsrc(Surv(comp_strength, censor) ~ ., data = data, ntree = 100)
 
 
-# This is my comment, this comment was written by Chris
-#hist(data$Name_1)
+
+
 

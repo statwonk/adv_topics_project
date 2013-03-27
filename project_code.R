536d90efeec608fddd7a454ac2572febbb4c6f1d
cpeter9 <Peters>
2013-03-26 23:07:58 -0500

    Cleaned up code.

--- project_code.R
+++ project_code.R
@@ -16,26 +16,33 @@ library(mboost)
 library(reshape2)
 
 
+# install.packages("MBA")
+library(MBA)
+
+# install.packages("rgl")
+library(rgl)
+
+# install.packages("akima")
+library(akima)
+
+# install.packages("randomForest")
+library(randomForest)
+
+
 data <- read.csv("Concrete_Data.csv") # this line enters data
 
 # Look at the data with this command
 # str(data)
 
+# Rename variables
 names(data) <- c("cement", "slag", "fly_ash", "water", "plasticizer", "coarse_aggregate", "fine_aggregate", "age", "comp_strength") # this renames variables
-
-head(data, 3) # This shows you the first 'x' number of rows of the data
-
 
 #############################
 # Visualization and graphing
 ############################
 
-
-
 # Simple pairs analysis
 # pairs(data) # many non-linear and weak relationships
-
-
 
 df <- melt(data, c("comp_strength")) # transforms data, comp_strength by every other variable, this is a useful form for plotting
 
@@ -59,8 +66,6 @@ ggplot(df, aes(x = comp_strength, y = value)) +
   facet_wrap(~ variable, scale = "free") +
   theme(strip.text.x = element_text(size = 14))
 
-# install.packages("MBA")
-library(MBA)
 
 var1 <- data$age
 var2 <- data$cement
@@ -71,10 +76,7 @@ output <- mba.surf(data.frame(list( x = var1, y = var2, z = data$comp_strength))
 #         xlab = "X", ylab = "Y", zlab = "Sinc( r )")
 
 
-library(rgl)
 
-# install.packages("akima")
-library(akima)
 
 # data
 rgl.spheres(var1 , data$comp_strength , var2, 3, color="red")
@@ -97,8 +99,7 @@ pct_training <- 0.9
 
 train <- sample(1:nrow(data), round(nrow(data) * pct_training))
 
-# install.packages("randomForest")
-library(randomForest)
+
 rf_tree <- randomForest(comp_strength ~ ., data = data[train, ], ntree = 500, importance = TRUE)
 
 # A look at variable relationships
@@ -106,9 +107,6 @@ partialPlot(rf_tree, x.var = "cement", pred.data = data, add = FALSE) # some int
 
 varImpPlot(rf_tree)
 
-###
-# How well does this model predict?
-###
 preds_rf <- predict(rf_tree, data[-train, -length(data)])
 
 plot(data[-train, "comp_strength"], preds_rf)
@@ -121,6 +119,7 @@ rf_output <- as.data.frame(list(rf_preds = preds_rf), stringsAsFactors = FALSE)
 ########
 # OLS
 #######
+
 ols <- lm(comp_strength ~ ., data = data)
 
 preds_ols <- predict(ols, data[-train, -length(data)])
@@ -153,36 +152,17 @@ gam_output <- as.data.frame(list(gam_preds = preds_gam), stringsAsFactors = FALS
 ################
 # Model comparisons
 ################
+
 output_combo <- cbind(ols_output, rf_output, gam_output, data[-train, "comp_strength"])
   names(output_combo)[length(output_combo)] <- "actuals"
 output_combo <- melt(output_combo, "actuals")
 
 ggplot(output_combo, aes(x = actuals, y = value, colour = variable)) +
-  geom_smooth() + 
-  stat_density2d(geom = "tile", aes(alpha = ..density..), contour = FALSE) +
-  geom_abline(intercept = 0, slope = 1)
+  geom_line(size = 2, alpha = 0.5) + 
+  # geom_smooth(method = "loess", span = 0.05) +
+  geom_abline(intercept = 0, slope = 1) +
+  scale_x_continuous(limits = c(0, 80)) +
+  scale_y_continuous(limits = c(0, 80))
 
-
-
-data(LIDAR)
-mba.int <- mba.surf(LIDAR, 300, 300, extend=TRUE)$xyz.est
-## Not run:
-##Image plot
-image(mba.int, xaxs="r", yaxs="r")
-##Perspective plot
-persp(mba.int, theta = 135, phi = 30, col = "green3", scale = FALSE,
-      ltheta = -120, shade = 0.75, expand = 10, border = NA, box = FALSE)
-##For a good time I recommend using rgl
-library(rgl)
-ex <- 10
-x <- mba.int[[1]]
-y <- mba.int[[2]]
-z <- ex*mba.int[[3]]
-zlim <- range(z)
-zlen <- zlim[2] - zlim[1] + 1
-colorlut <- heat.colors(as.integer(zlen))
-col <- colorlut[ z-zlim[1]+1 ]
-open3d()
-surface3d(x, y, z, color=col, back="lines")
 
 
